5ed09f3e93235471306cceab7caea95b1f728d8c
Robert Imangulov <robby300im@gmail.com>
2021-10-30 22:26:15 +0500

    Работает с костылями

--- src/main/java/com/example/myProject/controllers/FileUploadController.java
+++ src/main/java/com/example/myProject/controllers/FileUploadController.java
@@ -15,28 +15,28 @@ import org.springframework.web.multipart.MultipartFile;
 @Controller
 public class FileUploadController {
 
-    @RequestMapping(value="/upload", method=RequestMethod.GET)
-    public @ResponseBody String provideUploadInfo() {
+    @RequestMapping(value = "/upload", method = RequestMethod.GET)
+    public @ResponseBody
+    String provideUploadInfo() {
         return "Вы можете загружать файл с использованием того же URL.";
     }
 
-    @RequestMapping(value="/upload", method=RequestMethod.POST)
-    public @ResponseBody String handleFileUpload(@RequestParam("name") String name,
-                                                 @RequestParam("file") MultipartFile file){
+    @RequestMapping(value = "/upload", method = RequestMethod.POST)
+    public @ResponseBody
+    void handleFileUpload(@RequestParam("name") String name,
+                          @RequestParam("file") MultipartFile file) {
         if (!file.isEmpty()) {
             try {
                 byte[] bytes = file.getBytes();
                 BufferedOutputStream stream =
-                        new BufferedOutputStream(new FileOutputStream(new File("src/main/resources/static/images/" +name)));
+                        new BufferedOutputStream(new FileOutputStream(new File("src/main/resources/static/images/" + name)));
                 stream.write(bytes);
                 stream.close();
-                return "Вы удачно загрузили " + name + " в " + name + "-uploaded !";
-            } catch (Exception e) {
-                return "Вам не удалось загрузить " + name + " => " + e.getMessage();
-            }
-        } else {
-            return "Вам не удалось загрузить " + name + " потому что файл пустой.";
-        }
-    }
 
+            } catch (Exception e) {
+
+            }
+        }
+
+    }
 }
